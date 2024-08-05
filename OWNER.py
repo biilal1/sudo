9c50d8933f3f80c50b5b2930de3273f0b010ff46
01111555747999541 <ahmee4131@gmail.com>
2024-08-06 01:20:10 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["M_9_T", "N_7_k"]
+OWNER_NAME = "𓏺 َِ𝘾َِ𝘳- َِ𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
+BOT_TOKEN = "7202087709:AAHbDSbjT1-b_xoG0DCMh2EHN0h-DX5Kqg4"
+DATABASE = "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/vzo_a"
+GROUP = "https://t.me/cr_nox"
+VIDEO = "https://t.me/vzo_a/19"
+LOGS = ["M_9_T", "N_7_k"]
+
---
+++ bot.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from pyromod import listen
+
+
+
+bot = Client(
+    "mo",
+    api_id="15837209",
+    api_hash="fe081df6989a4d79c1004903bb4f23e6",
+    bot_token="7202087709:AAHbDSbjT1-b_xoG0DCMh2EHN0h-DX5Kqg4",
+    plugins=dict(root="Maker")
+    )
+
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    AFROTOO = "M_9_T"
+    await bot.send_message(AFROTOO, "**تم تشغيل ال صانع بنجاح عزيزي المطور ...🥀،**")
+    print("[INFO]: تم تشغيل الصانع وارسال رسالة للمطور⚡🚦.")
+    await idle()
