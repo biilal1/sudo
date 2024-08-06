877dafddd4544fe66d55a49ce6c0c16e0e252700
01111555747999541 <ahmee4131@gmail.com>
2024-08-07 00:57:43 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["M_9_T", "N_7_k"]
 OWNER_NAME = "𓏺 َِ𝘾َِ𝘳- َِ𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
-BOT_TOKEN = "7202087709:AAHbDSbjT1-b_xoG0DCMh2EHN0h-DX5Kqg4"
+BOT_TOKEN = "7202087709:AAF0vR9fHZsuJEVhLW9z5CsQEbQs7mgVq04"
 DATABASE = "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/vzo_a"
 GROUP = "https://t.me/cr_nox"
