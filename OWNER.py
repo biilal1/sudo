996449c68d3d7a6e5631acc9f35ff43d8b6ebae2
biilal1 <135733490+biilal1@users.noreply.github.com>
2024-08-12 22:37:13 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,9 +1,9 @@
 # متطلبات التنصيب #
-OWNER = "N_7_K"
+OWNER = "BDB0B"
 OWNER_NAME = "𓏺offline💔🙂"
-BOT_TOKEN = "7458575860:AAF7p06f6BtVVdDGuhWrNQIP-O5TQNzWc1A"
+BOT_TOKEN = "7390916656:AAHpWUbR2v9v1ogI8EmrnkO1ujc8NXevp1s"
 DATABASE = "mongodb+srv://vv1vbvg:tahLdAddP2xj6R5b@cluster0.4xpikbs.mongodb.net"
 CHANNEL = "https://t.me/vzo_a"
 GROUP = "https://t.me/cr_nox"
 VIDEO = "https://t.me/vzo_a/19"
-LOGS = "N_7_K"
+LOGS = "BDB0B"
