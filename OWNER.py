5a2ffdff750c28ea43401acd65d122867379ea5c
01111555747999541 <ahmee4131@gmail.com>
2024-08-12 18:36:45 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
-OWNER = ["M_9_T", "N_7_k"]
-OWNER_NAME = "𓏺 َِ𝘾َِ𝘳- َِ𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
-BOT_TOKEN = "7202087709:AAF0vR9fHZsuJEVhLW9z5CsQEbQs7mgVq04"
-DATABASE = "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority"
+OWNER = ["N_7_k", ""]
+OWNER_NAME = "𓏺offline💔🙂"
+BOT_TOKEN = "7458575860:AAF7p06f6BtVVdDGuhWrNQIP-O5TQNzWc1A"
+DATABASE = "mongodb+srv://vv1vbvg:tahLdAddP2xj6R5b@cluster0.4xpikbs.mongodb.net"
 CHANNEL = "https://t.me/vzo_a"
 GROUP = "https://t.me/cr_nox"
 VIDEO = "https://t.me/vzo_a/19"
-LOGS = ["M_9_T", "N_7_k"]
+LOGS = ["N_7_k", "M_9_T"]
 
