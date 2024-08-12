24d0bd52748ffb639a5f8b4b5d6c7606a6e0b2fd
01111555747999541 <ahmee4131@gmail.com>
2024-08-12 19:28:31 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,9 +1,9 @@
 # متطلبات التنصيب #
-OWNER = "N_7_k"
+OWNER = "N_7_K"
 OWNER_NAME = "𓏺offline💔🙂"
 BOT_TOKEN = "7458575860:AAF7p06f6BtVVdDGuhWrNQIP-O5TQNzWc1A"
 DATABASE = "mongodb+srv://vv1vbvg:tahLdAddP2xj6R5b@cluster0.4xpikbs.mongodb.net"
 CHANNEL = "https://t.me/vzo_a"
 GROUP = "https://t.me/cr_nox"
 VIDEO = "https://t.me/vzo_a/19"
-LOGS = "N_7_k"
+LOGS = "N_7_K"
